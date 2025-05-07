b558776887158ee42d1723ea625c4c886efb8efa
youssefarag2 <youssefarag19@gmail.com>
2025-05-08 00:51:34 +0300

    Restaurants repo skeleton

---
+++ backend/src/modules/restaurants/restaurants.repository.ts
@@ -0,0 +1,39 @@
+import prisma from "../../database/connect";
+import { Restaurant, Prisma } from "@prisma/client";
+
+
+class RestaurantRepository {
+    async findMany(options: {
+        where?: Prisma.RestaurantWhereInput;
+        skip?: number;
+        take?: number;
+        orderBy?: Prisma.RestaurantOrderByWithRelationInput
+    }) : Promise <Restaurant[]> {
+         return prisma.restaurant.findMany({
+            where: options.where,
+            skip: options.skip,
+            take: options.take,
+            orderBy: options.orderBy
+        })
+    }
+
+    async count(options: {
+        where?: Prisma.RestaurantWhereInput;
+    }) : Promise<number> {
+        return prisma.restaurant.count({
+            where: options.where
+        })
+    }
+
+    async findById(id: number) : Promise<Restaurant | null> {
+        return prisma.restaurant.findUnique({
+            where: {
+                restaurant_id: id
+            }
+        })
+    }
+
+    // ADD methods for create, update later
+}
+
+export default new RestaurantRepository();
