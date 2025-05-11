9333247dd8a26659f22b72a6dc8b22c37df78cd2
youssefarag2 <youssefarag19@gmail.com>
2025-05-11 03:58:32 +0300

    adding the route for creating restaurant

--- backend/src/modules/restaurants/restaurants.routes.ts
+++ backend/src/modules/restaurants/restaurants.routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import restaurantsController from "./restaurants.controller";
 import { validateRequest } from "../../middleware/request.validator";
 import { QueryRestaurantsDto } from "./dtos/query-restaurants.dto";
+import {authGuard} from "../../middleware/auth.guard";
+import {CreateRestaurantDto} from "./dtos/create-restaurant.dto";
 
 const router = Router()
 
@@ -15,6 +17,11 @@ router.get("/:restaurantID",
     restaurantsController.getOne)
 
 // Add POST, PUT, DELETE routes later for creation/updates
+
+router.post("/",
+    authGuard,
+    validateRequest(CreateRestaurantDto, "body"),
+    restaurantsController.Create)
 
 
 export default router;
